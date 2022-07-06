7dfef391d639148e624f2b011b4674a2aff69e53
renato-Aspire-E5-574 <remontanari@live.com>
2022-07-06 20:18:22 -0300

    analytics config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,8 +29,8 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
         Blade::aliasComponent('admin.components.message', 'message');
 
-        // $configuracoes = \App\Models\Configuracoes::find(1); 
-        // View()->share('configuracoes', $configuracoes);        
+        $configuracoes = \App\Models\Configuracoes::find(1); 
+        View()->share('configuracoes', $configuracoes);        
 
         Paginator::useBootstrap();
     }
