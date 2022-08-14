cb4a7c1bd760e2db2696769a34d040240550c95e
renato-Aspire-E5-574 <remontanari@live.com>
2022-08-14 19:35:34 -0300

    app service update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,13 +27,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // URL::forceScheme('https');
-        // Schema::defaultStringLength(191);
-        // Blade::aliasComponent('admin.components.message', 'message');
+        URL::forceScheme('https');
+        Schema::defaultStringLength(191);
+        Blade::aliasComponent('admin.components.message', 'message');
 
-        // $configuracoes = \App\Models\Configuracoes::find(1); 
-        // View()->share('configuracoes', $configuracoes);        
+        $configuracoes = \App\Models\Configuracoes::find(1); 
+        View()->share('configuracoes', $configuracoes);        
 
-        // Paginator::useBootstrap();
+        Paginator::useBootstrap();
     }
 }
