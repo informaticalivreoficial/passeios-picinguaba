9f5222cf3f6160245c49b53e72e0aa56aa66e3c3
renato-Aspire-E5-574 <renatomontanari@ymail.com>
2023-07-03 13:56:29 -0300

    update descrição view

--- resources/views/web/master/master.blade.php
+++ resources/views/web/master/master.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">  
         <meta name="language" content="pt-br" />  
-        <meta name="robots" content="index, follow"/>      
+        <meta name="robots" content="noindex"/>      
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="shortcut icon" type="image/x-icon" href="{{$configuracoes->getfaveicon()}}">
         <meta name="csrf-token" content="{{ csrf_token() }}">
